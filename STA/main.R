7c2152615ed6aad44f60b36d9f4453562b9679d3
Lisa Steinitz <lisa.steinitz@gmail.com>
2024-06-11 23:22:10 +0200

    fix x-axis

--- STA/main.R
+++ STA/main.R
@@ -6,19 +6,17 @@ abu
 kadaver <- read.csv2("5_6_Kadaverdaten_881a69df2606fb9e7a772a99779e2a56.CSV")
 kadaver
 
-
+#artentabelle erstellen
+arten<- data.frame(abu$Buteo_buteo, abu$Corvus_spec., abu$Corvus_corax)
 
 #Sortieren des Datensatzes nach Temperatur & Niederschlag
 abu <- abu[order(abu$Temperatur, abu$Niederschlag),]
-
-#artentabelle erstellen
-arten<- data.frame(abu$Buteo_buteo, abu$Corvus_spec., abu$Corvus_corax)
 
 #Temperatur& Niederschlagsdaten stimmen in Datensatz nicht mehr mit Ausgangswert überein
 # werden aber noch ordentlich angezeigt
 abu
 
-plot(arten, abu$Temperatur, type= "l", xlim= c(0,10),ylim= c(0,70), col= "red")
-lines(arten, abu$Niederschlag/2, type="l",col="blue")
+plot(abu$Buteo_buteo, abu$Temperatur, type= "l", xlim= c(0,10),ylim= c(0,70), col= "red")
+lines(abu$Buteo_buteo, abu$Niederschlag/2, type="l",col="blue")
 axis(4, at=seq(0,70,10), seq(0,140,20))
 
